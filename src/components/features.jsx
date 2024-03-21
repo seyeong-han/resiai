f8e14be3829cbfb7f5bcb83ef39440725a301e56
SeyeongHan <h960213@gmail.com>
2024-03-20 23:32:04 -0500

    feat: adopt features' css

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -11,18 +11,17 @@ export const Features = (props) => {
                     {props.data
                         ? props.data.map((d, i) => (
                               <div key={`${d.title}-${i}`} className="row">
-                                  <div className="col-xs-2 col-md-2 d-flex">
+                                  <div className="col-xs-4 col-md-4 d-flex">
                                       <div className="thumbnail">
                                           <img
                                               src={d.img}
                                               alt="..."
-                                              className="team-img"
+                                              className="feature-img"
                                           />{' '}
                                       </div>
                                   </div>
                                   <div
-                                      className="col-xs-10 col-md-10 justify-content-start align-items-start"
-                                      textAlign="left"
+                                      className="col-xs-8 col-md-8"
                                   >
                                       <h3>{d.title}</h3>
                                       <p>{d.text}</p>
